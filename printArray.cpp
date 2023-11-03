7de9e42a0f0bc2f708b409b47b66213ba2612728
Shanell Pereira <Shanell.pereira@yahoo.co.uk>
2023-11-03 22:00:36 +0000

    Added a new function

--- printArray.cpp
+++ printArray.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void print_array(int array[],int size){
+void print_array(int array[], int size){
     cout << "[";
     for (int i=0; i<= (size-1); i++){
         if (i != (size-1))
@@ -12,7 +12,20 @@ void print_array(int array[],int size){
     cout << "]";
 }
 
+void swap(int array[], int a, int b){
+    int x;
+    x = array[a];
+    array[a] = array[b];
+    array[b] = x;
+}
+
 int main(){
     int numbers[] = {1, 2, 3};
     print_array(numbers, 3);
+
+    cout << "\nAfter swap: " << std::endl;
+    swap(numbers, 0, 1);
+    print_array(numbers, 3);
+
+
 }
